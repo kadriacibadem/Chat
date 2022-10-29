50444f9cfdc41dbffe9d6544854872cbba2a3855
Ali Öztürk <84031734+traaz@users.noreply.github.com>
2022-10-29 21:34:46 +0300

    Update client.py

--- Chat/client.py
+++ Chat/client.py
@@ -1,6 +1,9 @@
+import os.path
+import tkinter
 from socket import *
 from threading import *
 from tkinter import *
+from tkinter import filedialog as fd
 
 client = socket(AF_INET, SOCK_STREAM)
 ip= '127.0.0.1'
@@ -9,7 +12,7 @@ port = 8080
 client.connect((ip, port))
 
 pencere = Tk()
-pencere.title('Baglandi: ' + ip + ':' + str(port))
+pencere.title('Chat')
 
 messages =Text(pencere, width=50)
 messages.grid(row=0, column=0, padx=10, pady=10)
@@ -25,9 +28,19 @@ def sendMessage():
     client.send(clientMessage.encode('utf8'))
     yourMessage.delete(0, END)
 
+def openFile():
+    filepath= fd.askopenfilename()
+    file=open(filepath)
+    name = os.path.basename(file.name)
+    messages.insert(END, '\n'+ 'Sen: ' + name)
 
-bmessageGonder = Button(pencere, text= 'Gonder', width=20,command=sendMessage)
-bmessageGonder.grid(row=2,column=0,padx=10,pady=10)
+
+bmessageGonder = Button(pencere, text= 'Gonder',command=sendMessage)
+bmessageGonder.grid(row=2,column=0,padx=5,pady=5)
+
+dosyaSec = Button(pencere, text = 'Dosya Sec', command=openFile)
+dosyaSec.grid(row=3,column=0,padx=5,pady=5)
+
 
 
 def recvMessage():
